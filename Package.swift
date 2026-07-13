067ea820a4bf4e9ac3a3201398241f06fa01ba62
Kresna <67815088+kresna1174@users.noreply.github.com>
2026-07-13 23:12:05 +0700

    feat: scaffold SPM project with dependencies

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "ClipboardManager",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.0.0"),
+        .package(url: "https://github.com/soffes/HotKey.git", from: "0.2.0"),
+        .package(url: "https://github.com/krisk/fuse-swift.git", from: "1.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "ClipboardManager",
+            dependencies: [
+                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "HotKey", package: "HotKey"),
+                .product(name: "Fuse", package: "fuse-swift"),
+            ],
+            path: "Sources"
+        )
+    ]
+)
